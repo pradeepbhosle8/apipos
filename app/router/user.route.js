fbcd5bac318dbe21bd974fbf8f9988d2ec2bec6a
Dev Server <pradeep.bhosle8@gmail.com>
2022-02-25 20:07:19 +0530

    user router changes

--- app/router/user.route.js
+++ app/router/user.route.js
@@ -7,19 +7,19 @@ const auth = require('../middleware/auth');
 
 
 // create 
-router.post('/', userController.createUser);
+router.post('/', auth, userController.createUser);
 
 // get user
 router.get('/', auth, userController.findAllUser);
 
 // single user get
-router.get('/:id', userController.findOneUser);
+router.get('/:id', auth, userController.findOneUser);
 
 // update user
-router.put('/:id', userController.updateUser);
+router.put('/:id', auth, userController.updateUser);
 
 // delete user 
-router.delete('/:id', userController.destroyUser);
+router.delete('/:id', auth, userController.destroyUser);
 
 // SignIn Up Or Login 
 router.post('/v2/login', userController.signIn);
